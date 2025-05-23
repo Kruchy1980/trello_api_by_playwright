3b782e312b353f671c20ba05d131ea42e12e26e6
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-05-23 13:45:43 +0200

    feat: Auth-verification test added

---
+++ tests/auth-verification.spec.ts
@@ -0,0 +1,13 @@
+import { API_KEY, TOKEN } from '@_config/env.config';
+import { expect, test } from '@playwright/test';
+
+test('Authorization verification', async ({ request }) => {
+  // Arrange:
+  const expectedStatusCode = 200;
+  // Act: curl '/1/members/me/boards?key={yourKey}&token={yourToken}'
+  const response = await request.get(
+    `/1/members/me/boards?key=${API_KEY}&token=${TOKEN}`,
+  );
+  // Assert:
+  expect(response.status()).toEqual(expectedStatusCode);
+});
