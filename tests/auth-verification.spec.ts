f4ce171eea73f45f806c8c803c99d657972eaac8
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-05-27 14:08:16 +0200

    update: first api call format added

--- tests/auth-verification.spec.ts
+++ tests/auth-verification.spec.ts
@@ -4,12 +4,10 @@ import { expect, test } from '@playwright/test';
 test('Authorization verification', async ({ request }) => {
   // Arrange:
   const expectedStatusCode = 200;
-
   // Act:
   const response = await request.get(
     `/1/members/me/boards?key=${API_KEY}&token=${TOKEN}`,
   );
-
   // Assert:
   expect(response.status()).toEqual(expectedStatusCode);
 });
