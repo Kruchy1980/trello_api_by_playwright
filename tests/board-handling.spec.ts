a2a3b44e99502277b7592651a84a07b925bdb8cc
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-05-27 18:07:57 +0200

    wip: Boards handling update and get a field from board tests added

--- tests/board-handling.spec.ts
+++ tests/board-handling.spec.ts
@@ -47,4 +47,47 @@ test.describe('Boards handling - dependent tests', () => {
     const actualBoardDescription = responseJSON.desc;
     expect(actualBoardDescription).toContain(expectedBoardDescription);
   });
+  test('3. Should Update a board', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const updatedBoardName = 'My first board name - updated';
+    const updatedBoardDescription = 'My first board description - updated';
+    const expectedBoardId = createdBoardId;
+    // Opcjonalny header
+    const headers = { 'Content-Type': 'application/json' };
+    // Act: 'https://api.trello.com/1/boards/{id}?key=APIKey&token=APIToken'
+    const response = await request.put(
+      `/1/boards/${expectedBoardId}?name=${updatedBoardName}&desc=${updatedBoardDescription}&key=${API_KEY}&token=${TOKEN}`,
+      { headers },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    const actualBoardId = responseJSON.id;
+    expect(actualBoardId).toContain(expectedBoardId);
+    const actualBoardName = responseJSON.name;
+    expect(actualBoardName).toContain(updatedBoardName);
+    const actualBoardDescription = responseJSON.desc;
+    expect(actualBoardDescription).toContain(updatedBoardDescription);
+  });
+  test('4. Should Get a field on a Board', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const updatedBoardName = 'My first board name - updated';
+    const expectedBoardId = createdBoardId;
+    // Opcjonalny header
+    const headers = { Accept: 'application/json' };
+    // Act: 'https://api.trello.com/1/boards/{id}/{field}?key=APIKey&token=APIToken'
+    const response = await request.get(
+      `/1/boards/${expectedBoardId}/name?key=${API_KEY}&token=${TOKEN}`,
+      { headers },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    const actualBoardName = responseJSON._value;
+    expect(actualBoardName).toContain(updatedBoardName);
+  });
 });
