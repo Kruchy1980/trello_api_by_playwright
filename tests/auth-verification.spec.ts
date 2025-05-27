b0b29ff6ff83eeaf4cb4846adb6a55f0c4e8d9e4
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-05-27 10:16:33 +0200

    update: Auth verification test updated

--- tests/auth-verification.spec.ts
+++ tests/auth-verification.spec.ts
@@ -4,7 +4,7 @@ import { expect, test } from '@playwright/test';
 test('Authorization verification', async ({ request }) => {
   // Arrange:
   const expectedStatusCode = 200;
-  // Act: curl '/1/members/me/boards?key={yourKey}&token={yourToken}'
+  // Act:  // 'https://api.trello.com/1/members/me/boards?key={yourKey}&token={yourToken}'
   const response = await request.get(
     `/1/members/me/boards?key=${API_KEY}&token=${TOKEN}`,
   );
