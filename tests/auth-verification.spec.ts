3f215aada937949f8b43f003daa3316a8994cdf7
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-06-03 19:21:33 +0200

    feat: auth verification test added

--- tests/auth-verification.spec.ts
+++ tests/auth-verification.spec.ts
@@ -4,7 +4,8 @@ import { expect, test } from '@playwright/test';
 test('Authorization verification', async ({ request }) => {
   // Arrange:
   const expectedStatusCode = 200;
-  // Act:
+
+  // Act: curl 'https://api.trello.com/1/members/me/boards?key={yourKey}&token={yourToken}'
   const response = await request.get(
     `/1/members/me/boards?key=${API_KEY}&token=${TOKEN}`,
   );
