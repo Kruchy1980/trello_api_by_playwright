a34b389f53a4dde6c97aba0e08c3d76216a8451a
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-06-04 19:25:31 +0200

    update: boards-handling tests updated delete, get deleted and unauthorized

--- tests/boards-handling.spec.ts
+++ tests/boards-handling.spec.ts
@@ -79,4 +79,87 @@ test.describe('Boards handling - dependent tests', () => {
     const actualBoardDescription = responseJSON.desc;
     expect(actualBoardDescription).toContain(updatedBoardDescription);
   });
+  test('4. Should get a field from board', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const updatedBoardName = 'Updated Board name';
+    //Optional headers
+    const headers = { Accept: 'application/json' };
+
+    // Act: 'https://api.trello.com/1/boards/{id}/{field}?key=APIKey&token=APIToken'
+    const response = await request.get(
+      `/1/boards/${createdBoardId}/name?key=${API_KEY}&token=${TOKEN}`,
+      { headers },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+
+    const actualBoardName = responseJSON._value;
+    expect(actualBoardName).toContain(updatedBoardName);
+  });
+  test('5. Should Delete a board', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const expectedResponseValue = null;
+
+    //Optional headers
+    const headers = { Content_Type: 'application/json' };
+
+    // Act: 'https://api.trello.com/1/boards/{id}?key=APIKey&token=APIToken'
+    const response = await request.delete(
+      `/1/boards/${createdBoardId}/?key=${API_KEY}&token=${TOKEN}`,
+      { headers },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+
+    const actualResponseValue = responseJSON._value;
+    expect(actualResponseValue).toEqual(expectedResponseValue);
+  });
+  test('6. (NP) Should NOT get a deleted board', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 404;
+    const expectedStatusText = 'Not Found';
+
+    //Optional headers
+    const headers = { Accept: 'application/json' };
+
+    // Act: 'https://api.trello.com/1/boards/{id}?key=APIKey&token=APIToken'
+    const response = await request.get(
+      `/1/boards/${createdBoardId}?key=${API_KEY}&token=${TOKEN}`,
+      { headers },
+    );
+    // console.log(response);
+
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    expect(response.statusText()).toContain(expectedStatusText);
+  });
+  test('7. (NP) Should Not get board when unauthorized user', async ({
+    request,
+  }) => {
+    // Arrange:
+    const expectedStatusCode = 401;
+    const expectedStatusText = 'Unauthorized';
+
+    //Optional headers
+    const headers = { Accept: 'application/json' };
+
+    // Act: 'https://api.trello.com/1/boards/{id}?key=APIKey&token=APIToken'
+    const response = await request.get(
+      `/1/boards/${createdBoardId}?key=poisfbnzpoib&token=${TOKEN}`,
+      { headers },
+    );
+    // console.log(response);
+
+    // Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    expect(response.statusText()).toContain(expectedStatusText);
+  });
 });
