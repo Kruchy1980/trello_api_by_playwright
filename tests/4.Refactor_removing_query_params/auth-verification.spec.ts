b7d73c2b7372174b8d3dc812d6156a825fd9eee9
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-08-19 14:20:27 +0200

    update: already pushed tests repush

---
+++ tests/4.Refactor_removing_query_params/auth-verification.spec.ts
@@ -0,0 +1,19 @@
+import { headers, params } from '@_src/API/utils/api_utils';
+import { expect, test } from '@playwright/test';
+
+// TODO: For refactoring
+// TODO: Simplify the URLS generation
+
+test('Authorization verification', async ({ request }) => {
+  // Arrange:
+  const expectedStatusCode = 200;
+
+  // Act: 'https://api.trello.com/1/members/me/boards?key={yourKey}&token={yourToken}'
+  const response = await request.get(`/1/members/me/boards`, {
+    headers,
+    params,
+  });
+
+  // Assert:
+  expect(response.status()).toEqual(expectedStatusCode);
+});
