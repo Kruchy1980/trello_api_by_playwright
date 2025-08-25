7929354c978e59c44722ede5a149ad1cc3993a71
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-08-25 14:49:36 +0200

    update: models_from_previous article added

---
+++ src/API/models/board-data.model.ts
@@ -0,0 +1,14 @@
+// export interface BoardDataCreationModel {
+//   name: string;
+//   desc?: string;
+// }
+
+// export interface BoardDataUpdateModel {
+//   name?: string;
+//   desc?: string;
+// }
+
+export interface BoardDataModel {
+  name?: string;
+  desc?: string;
+}
