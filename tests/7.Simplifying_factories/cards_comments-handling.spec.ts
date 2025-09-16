bf0aec039c7d5224a7e0b8c7702b9f75ab6ae0b1
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-09-16 17:39:56 +0200

    feat: card comments tests simplified factory usage

---
+++ tests/7.Simplifying_factories/cards_comments-handling.spec.ts
@@ -0,0 +1,203 @@
+import { prepareRandomBoardDataSimplified } from '@_src/API/factories/simplified_factories/board-data.factory';
+import { prepareRandomCommentDataSimplified } from '@_src/API/factories/simplified_factories/card_comments-data.factory';
+import { prepareRandomCardDataSimplified } from '@_src/API/factories/simplified_factories/cards-data.factory';
+import { BoardDataModel } from '@_src/API/models/board-data.model';
+import { CardDataModel } from '@_src/API/models/card-data.model';
+import { CardCommentDataModel } from '@_src/API/models/card_comments-data.model';
+import { ParamsDataModel } from '@_src/API/models/params-data.model';
+import { headers, params } from '@_src/API/utils/api_utils';
+import { expect, test } from '@playwright/test';
+import { prepareParamsData } from 'future/7.Refactor_simplifying_factories/part_1_finished/factories/params-data.factory';
+
+// TODO: For refactoring
+// TODO: Make the models and factories simpler to use
+// TODO: Prepare functions for generate URLS
+// TODO: Simplify the URLS generation
+
+test.describe('Cards comments handling - simplified factories', () => {
+  let createdBoardId: string;
+  const createdListsIds: string[] = [];
+  let createdCardId: string;
+  let commentActionId: string;
+  // Added for verification
+  let createdCommentText: string;
+
+  test.beforeAll(
+    'Board, card preparation and collect lists ids',
+    async ({ request }) => {
+      // Arrange:
+      const data: BoardDataModel = prepareRandomBoardDataSimplified();
+
+      // Act: 'https://api.trello.com/1/boards/?name={name}&key=APIKey&token=APIToken'
+      const response = await request.post(`/1/boards`, {
+        headers,
+        params,
+        data,
+      });
+      const responseJSON = await response.json();
+      // console.log(responseJSON);
+      createdBoardId = responseJSON.id;
+
+      // Collect lists Id's
+      // Act: 'https://api.trello.com/1/boards/{id}/lists?key=APIKey&token=APIToken'
+      const responseListsIds = await request.get(
+        `/1/boards/${createdBoardId}/lists`,
+        {
+          headers,
+          params,
+        },
+      );
+      const responseListsIdsJSON = await responseListsIds.json();
+      // console.log(responseJSON);
+      responseListsIdsJSON.forEach((listId: { id: string }) => {
+        createdListsIds.push(listId.id);
+      });
+
+      // Card Preparation
+      // Arrange:
+      const cardCreationData: CardDataModel = prepareRandomCardDataSimplified(
+        createdListsIds[0],
+        '',
+        undefined,
+        undefined,
+        undefined,
+        true,
+      );
+
+      // Act: 'https://api.trello.com/1/cards?idList=5abbe4b7ddc1b351ef961414&key=APIKey&token=APIToken'
+      const responseCardCreation = await request.post(`/1/cards`, {
+        headers,
+        params,
+        data: cardCreationData,
+      });
+      const responseCardCreationJSON = await responseCardCreation.json();
+      // console.log(responseJSON);
+      createdCardId = responseCardCreationJSON.id;
+    },
+  );
+  test.beforeEach('Create a new comment on a card', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const expectedCommentType = 'comment';
+
+    const data: CardCommentDataModel = prepareRandomCommentDataSimplified(
+      '',
+      2,
+    );
+
+    // Act: 'https://api.trello.com/1/cards/{id}/actions/comments?text={text}&key=APIKey&token=APIToken'
+    const response = await request.post(
+      `/1/cards/${createdCardId}/actions/comments`,
+      { headers, params, data },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+    // console.log(responseJSON.entities);
+    commentActionId = responseJSON.id;
+
+    //Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    expect(responseJSON.entities[0]).toHaveProperty('id');
+    const actualCardCommentType =
+      responseJSON.entities[responseJSON.entities.length - 1].type;
+    expect(actualCardCommentType).toContain(expectedCommentType);
+    const actualCardCommentText =
+      responseJSON.entities[responseJSON.entities.length - 1].text;
+    expect(actualCardCommentText).toContain(data.text);
+    // Add text of comment to global variable
+    createdCommentText = data.text;
+  });
+
+  test('1. Should get a comment on a card', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const expectedCardCommentText = createdCommentText;
+
+    const commentCardParams: ParamsDataModel = prepareParamsData(
+      '',
+      '',
+      '',
+      '',
+      false,
+      'commentCard',
+    );
+    // console.log(commentCardParams);
+
+    // Act: 'https://api.trello.com/1/cards/{id}/actions?key=APIKey&token=APIToken'
+    const response = await request.get(`/1/cards/${createdCardId}/actions`, {
+      headers,
+      params: { ...params, ...commentCardParams },
+    });
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+    // commentActionId = responseJSON[0].id;
+
+    //Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    const actualCardCommentText = responseJSON[0].data.text;
+    expect(actualCardCommentText).toContain(expectedCardCommentText);
+  });
+  test('2. Should update a comment action on a card', async ({ request }) => {
+    // Arrange:
+    const expectedStatusCode = 200;
+    const data: CardCommentDataModel = prepareRandomCommentDataSimplified(
+      'Updated ',
+      3,
+    );
+    // Act: 'https://api.trello.com/1/cards/{id}/actions/{idAction}/comments?text={text}&key=APIKey&token=APIToken'
+    const response = await request.put(
+      `/1/cards/${createdCardId}/actions/${commentActionId}/comments`,
+      { headers, params, data },
+    );
+    const responseJSON = await response.json();
+    // console.log(responseJSON);
+
+    //Assert:
+    expect(response.status()).toEqual(expectedStatusCode);
+    const actualCardCommentId = responseJSON.id;
+    expect(actualCardCommentId).toContain(commentActionId);
+    const actualCardCommentText = responseJSON.data.text;
+    expect(actualCardCommentText).toContain(data.text);
+  });
+
+  test('3. Delete comment action and verify whether source exists', async ({
+    request,
+  }) => {
+    await test.step('3.1 Should Delete a comment action on a card', async () => {
+      // Arrange:
+      const expectedStatusCode = 200;
+      const expectedResponseValue = null;
+      // Act: 'https://api.trello.com/1/cards/{id}/actions/{idAction}/comments?key=APIKey&token=APIToken'
+      const response = await request.delete(
+        `/1/cards/${createdCardId}/actions/${commentActionId}/comments`,
+        { headers, params },
+      );
+      const responseJSON = await response.json();
+      // console.log(responseJSON);
+
+      //Assert:
+      expect(response.status()).toEqual(expectedStatusCode);
+      const actualResponseValue = responseJSON._value;
+      expect(actualResponseValue).toEqual(expectedResponseValue);
+    });
+    await test.step('3.2 (NP) Should Not Get deleted comment Action', async () => {
+      // Arrange:
+      const expectedStatusCode = 404;
+      const expectedResponseText = 'Not Found';
+
+      // Act: 'https://api.trello.com/1/actions/{id}?key=APIKey&token=APIToken'
+      const response = await request.get(`/1/actions/${commentActionId}`, {
+        headers,
+        params,
+      });
+
+      //Assert:
+      expect(response.status()).toEqual(expectedStatusCode);
+      expect(response.statusText()).toEqual(expectedResponseText);
+    });
+  });
+  test.afterAll('Delete a board', async ({ request }) => {
+    // Act: 'https://api.trello.com/1/boards/{id}?key=APIKey&token=APIToken'
+    await request.delete(`/1/boards/${createdBoardId}`, { headers, params });
+  });
+});
