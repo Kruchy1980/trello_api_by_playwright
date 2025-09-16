a897d3ce7fa25605c291241488596b97ef12e096
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-09-16 19:27:05 +0200

    update: old version of card labels data model commented

--- src/API/factories/cards_labels-data.factory.ts
+++ src/API/factories/cards_labels-data.factory.ts
@@ -1,121 +1,121 @@
-import {
-  CardLabelDataModel,
-  LabelDataModel,
-  LabelOperationsDataModel,
-} from '@_src/API/models/card_labels-data.model';
-import { faker } from '@faker-js/faker';
+// import {
+//   CardLabelDataModel,
+//   LabelDataModel,
+//   LabelOperationsDataModel,
+// } from '@_src/API/models/card_labels-data.model';
+// import { faker } from '@faker-js/faker';
 
-// // For Labels Created For Board
-// export interface LabelDataModel {
-//   color: string;
-//   name: string;
-//   idBoard: string;
+// // // For Labels Created For Board
+// // export interface LabelDataModel {
+// //   color: string;
+// //   name: string;
+// //   idBoard: string;
+// // }
+
+// export function prepareRandomBoardLabelData(
+//   idBoard: string,
+//   customColor?: string,
+//   labelName?: string,
+//   words?: number,
+// ): LabelDataModel {
+//   // Solution II with choice of random color method
+//   // Prepare method which generates random color from array of colors
+//   const randomColorGenerator = (): string => {
+//     // INFO: Just Because Trello do not accept any random colors than example list is prepared without using faker
+//     const colors = [
+//       'green',
+//       'yellow',
+//       'red',
+//       'orange',
+//       'purple',
+//       'blue',
+//       'sky',
+//       'lime',
+//       'pink',
+//       'black',
+//     ];
+//     const randomColorIndex = Math.floor(Math.random() * colors.length);
+//     return colors[randomColorIndex];
+//   };
+//   const color: string = !customColor ? randomColorGenerator() : customColor;
+
+//   const name: string = labelName
+//     ? `${labelName} - ${faker.lorem.words(words ?? 1)} - ${color}`
+//     : `${faker.lorem.words(words ?? 1)} - ${color}`;
+
+//   const randomBoardLabelData: LabelDataModel = {
+//     color,
+//     name,
+//     idBoard,
+//   };
+
+//   return randomBoardLabelData;
 // }
 
-export function prepareRandomBoardLabelData(
-  idBoard: string,
-  customColor?: string,
-  labelName?: string,
-  words?: number,
-): LabelDataModel {
-  // Solution II with choice of random color method
-  // Prepare method which generates random color from array of colors
-  const randomColorGenerator = (): string => {
-    // INFO: Just Because Trello do not accept any random colors than example list is prepared without using faker
-    const colors = [
-      'green',
-      'yellow',
-      'red',
-      'orange',
-      'purple',
-      'blue',
-      'sky',
-      'lime',
-      'pink',
-      'black',
-    ];
-    const randomColorIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomColorIndex];
-  };
-  const color: string = !customColor ? randomColorGenerator() : customColor;
+// // // For Labels Created On Card Directly
+// // export interface CardLabelDataModel {
+// //   color: string;
+// //   name?: string;
+// // }
 
-  const name: string = labelName
-    ? `${labelName} - ${faker.lorem.words(words ?? 1)} - ${color}`
-    : `${faker.lorem.words(words ?? 1)} - ${color}`;
+// export function prepareRandomCardLabelData(
+//   customColor?: string,
+//   labelName?: string,
+//   words?: number,
+// ): CardLabelDataModel {
+//   // Solution I with faker - not recommended - faker generates colors not maintained by TRELLO app
+//   //   const color: string = customColor ? customColor : faker.color.human();
 
-  const randomBoardLabelData: LabelDataModel = {
-    color,
-    name,
-    idBoard,
-  };
+//   // Solution II with choice of random color method
+//   // Prepare method which generates random color from array of colors
+//   const randomColorGenerator = (): string => {
+//     // INFO: Just Because Trello do not accept any random colors than example list is prepared without using faker
+//     const colors = [
+//       'green',
+//       'yellow',
+//       'red',
+//       'orange',
+//       'purple',
+//       'blue',
+//       'sky',
+//       'lime',
+//       'pink',
+//       'black',
+//     ];
+//     const randomColorIndex = Math.floor(Math.random() * colors.length);
+//     return colors[randomColorIndex];
+//   };
 
-  return randomBoardLabelData;
-}
+//   const color: string = !customColor ? randomColorGenerator() : customColor;
 
-// // For Labels Created On Card Directly
-// export interface CardLabelDataModel {
-//   color: string;
-//   name?: string;
+//   const name: string =
+//     labelName && labelName !== ''
+//       ? `${labelName} - ${faker.lorem.words(words ?? 1)} - ${color}`
+//       : labelName === ''
+//         ? `${faker.lorem.words(words ?? 1)} - ${color}`
+//         : '';
+
+//   const randomCardLabelData: CardLabelDataModel = {
+//     color,
+//     ...(name !== '' && { name }),
+//   };
+
+//   return randomCardLabelData;
 // }
 
-export function prepareRandomCardLabelData(
-  customColor?: string,
-  labelName?: string,
-  words?: number,
-): CardLabelDataModel {
-  // Solution I with faker - not recommended - faker generates colors not maintained by TRELLO app
-  //   const color: string = customColor ? customColor : faker.color.human();
+// // // For Adding Label To Card Only
+// // export interface LabelOperationsDataModel {
+// //   value?: string;
+// // }
 
-  // Solution II with choice of random color method
-  // Prepare method which generates random color from array of colors
-  const randomColorGenerator = (): string => {
-    // INFO: Just Because Trello do not accept any random colors than example list is prepared without using faker
-    const colors = [
-      'green',
-      'yellow',
-      'red',
-      'orange',
-      'purple',
-      'blue',
-      'sky',
-      'lime',
-      'pink',
-      'black',
-    ];
-    const randomColorIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomColorIndex];
-  };
+// export function prepareOperationData(
+//   element: string,
+// ): LabelOperationsDataModel {
+//   const value = element;
 
-  const color: string = !customColor ? randomColorGenerator() : customColor;
+//   const chosenOperationData: LabelOperationsDataModel = {
+//     value,
+//   };
 
-  const name: string =
-    labelName && labelName !== ''
-      ? `${labelName} - ${faker.lorem.words(words ?? 1)} - ${color}`
-      : labelName === ''
-        ? `${faker.lorem.words(words ?? 1)} - ${color}`
-        : '';
-
-  const randomCardLabelData: CardLabelDataModel = {
-    color,
-    ...(name !== '' && { name }),
-  };
-
-  return randomCardLabelData;
-}
-
-// // For Adding Label To Card Only
-// export interface LabelOperationsDataModel {
-//   value?: string;
+//   return chosenOperationData;
 // }
-
-export function prepareOperationData(
-  element: string,
-): LabelOperationsDataModel {
-  const value = element;
-
-  const chosenOperationData: LabelOperationsDataModel = {
-    value,
-  };
-
-  return chosenOperationData;
-}
