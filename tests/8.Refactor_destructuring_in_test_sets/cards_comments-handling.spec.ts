a8bb640304b0f3a69f8f32c546161b808fa6ef6c
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-09-22 13:36:45 +0200

    update: missed names of passed values corrected

--- tests/8.Refactor_destructuring_in_test_sets/cards_comments-handling.spec.ts
+++ tests/8.Refactor_destructuring_in_test_sets/cards_comments-handling.spec.ts
@@ -37,12 +37,12 @@ test.describe('Cards comments handling - destructured', () => {
       const responseJSON = await response.json();
       // console.log(responseJSON);
       // Destructuring responseJSON object
-      const { id: expectedBoardId } = responseJSON;
+      const { id: actualBoardId } = responseJSON;
       // === Value to variable Assigning ===
       // Before destructuring
       // createdBoardId = responseJSON.id;
       // After destructuring
-      createdBoardId = expectedBoardId;
+      createdBoardId = actualBoardId;
 
       // Collect lists Id's
       // Act: 'https://api.trello.com/1/boards/{id}/lists?key=APIKey&token=APIToken'
