a8c467018d99ed71c2a8a0708862ffa2b2409e82
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-10-18 15:58:50 +0200

    update: corrected path params object corrected

--- src/API/utils/path_parameters_utils.ts
+++ src/API/utils/path_parameters_utils.ts
@@ -1,9 +1,9 @@
 export const pathParameters: { [key: string]: string } = {
-  boardParameter: '/1/boards/',
-  listParameter: '/1/lists/',
-  cardParameter: '/1/cards/',
-  checklistParameter: '/1/checklists/',
-  actionsParameter: '/1/actions/',
-  labelParameter: '/1/labels/',
-  memberParameter: '/1/members/',
+  boardParameter: '/1/boards',
+  listParameter: '/1/lists',
+  cardParameter: '/1/cards',
+  checklistParameter: '/1/checklists',
+  actionsParameter: '/1/actions',
+  labelParameter: '/1/labels',
+  memberParameter: '/1/members',
 };
